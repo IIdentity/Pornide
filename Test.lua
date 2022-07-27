60e802c9346b86d7fa9b05ee5e27049b64384ac9
IIdentity <107352642+IIdentity@users.noreply.github.com>
2022-07-28 00:30:45 +0200

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,3 +1,1 @@
-local a={["6403373529"] = "https://raw.githubusercontent.com/IIdentity/Pornide/main/Slap-Battles.lua"}
-
-if a[game.PlaceId]then loadstring(game:HttpGet(a[game.PlaceId]))()end
+local a={["6403373529"] = "https://raw.githubusercontent.com/IIdentity/Pornide/main/Slap-Battles.lua"}if a[game.PlaceId]then loadstring(game:HttpGet(a[game.PlaceId]))()end
